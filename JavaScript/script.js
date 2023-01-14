de1c5b57dd9f0ecdc610b46f3cb9ab35a98f16d2
jmcdlungren <j.mcd.lungren@gmail.com>
2023-01-13 19:11:12 -0700

    added in letters & symbols and methods - working on randomizing input

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -10,8 +10,22 @@ function writePassword() {
 
 }
 
+var capltr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
+var lwrltr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var sym = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", '{", "}', '~', "`", '"' ];
+
+const options = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", '{", "}', '~', "`", '"', "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+var indexcap = Math.floor(Math.random() * capltr.length);
+var indexlwr = Math.floor(Math.random() * lwrltr.length);
+var indexsym = Math.floor(Math.random() * sym.length);
+
+console.log(
+  capltr[indexcap], lwrltr[indexlwr], sym[indexsym]
+);
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);

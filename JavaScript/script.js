7a4c33af33e8b254e357c9f3f0ff1dff7aa45017
jmcdlungren <j.mcd.lungren@gmail.com>
2023-01-13 22:22:50 -0700

    removed console.log code from JS as no longer needed

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -17,9 +17,9 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
-  var capltr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var capLtr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-  var lwrltr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var lwrLtr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
   var sym = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", '{', "}", '~', "`", '"'];
 
@@ -29,19 +29,19 @@ function generatePassword() {
 
   if (passwordLength >=8 && passwordLength <=128) {
 
-  var capltrChecker = confirm("Please confirm if you would like capital letters.");
-  var lwrltrChecker = confirm("Please confirm if you would like lowercase letters.");
+  var capLtrChecker = confirm("Please confirm if you would like capital letters.");
+  var lwrLtrChecker = confirm("Please confirm if you would like lowercase letters.");
   var symChecker = confirm("Please confirm if you would like symbols.");
   var numChecker = confirm("Please confirm if you would like numbers.");
 
   const finalArrayChoices = [];
 
-  if (capltrChecker === true) {
-    finalArrayChoices.push.apply(finalArrayChoices, capltr);
+  if (capLtrChecker === true) {
+    finalArrayChoices.push.apply(finalArrayChoices, capLtr);
   }
 
-  if (lwrltrChecker === true) {
-    finalArrayChoices.push.apply(finalArrayChoices, lwrltr);
+  if (lwrLtrChecker === true) {
+    finalArrayChoices.push.apply(finalArrayChoices, lwrLtr);
   }
 
   if (symChecker === true) {
@@ -57,8 +57,6 @@ function generatePassword() {
   for (i = 0; i < passwordLength; i++) {
     finalPassword += finalArrayChoices[Math.floor(Math.random() * finalArrayChoices.length)]
   };
-
-  console.log(finalPassword.length);
 
   return finalPassword;
   } else {
